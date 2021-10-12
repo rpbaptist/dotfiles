f17cd853637aa6526b8edb8e34080f3fe096453b
Richard Baptist <code@richardbaptist.nl>
2021-10-12 18:00:47 +0200

    Add Stackline config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,25 @@
+stackline = require "stackline"
+
+local StacklineConfig = {
+  appearance = {
+    color = {red = 0.4, green = 0.4, blue = 0.4},
+    -- color = {red = 0.92, green = 0.86, blue = 0.70},
+    alpha = 0.8,
+    dimmer = 3,
+    iconDimmer = 1.5,
+    showIcons = true,
+    size = 22,
+    radius = 8,
+    iconPadding = 1,
+    pillThinness = 6,
+    vertSpacing = 1.2,
+    offset = {
+      x = 2,
+      y = 0
+    },
+    shouldFade = true,
+    fadeDuration = 0.1
+  },
+}
+
+stackline:init(StacklineConfig)
