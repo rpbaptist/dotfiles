712303e633732afc836fe5a09f6ad06e9b84e0fd
Richard Baptist <code@richardbaptist.nl>
2021-10-12 18:00:47 +0200

    Update stackline config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,15 +2,16 @@ stackline = require "stackline"
 
 local StacklineConfig = {
   appearance = {
-    color = {red = 0.4, green = 0.4, blue = 0.4},
-    -- color = {red = 0.92, green = 0.86, blue = 0.70},
+    -- color = {red = 0.11, green = 0.13, blue = 0.07},
+    -- color = {red = 0.4, green = 0.4, blue = 0.4},
+    color = {red = 0.92, green = 0.86, blue = 0.70},
     alpha = 0.8,
     dimmer = 3,
     iconDimmer = 1.5,
     showIcons = true,
     size = 22,
     radius = 8,
-    iconPadding = 1,
+    iconPadding = 0,
     pillThinness = 6,
     vertSpacing = 1.2,
     offset = {
