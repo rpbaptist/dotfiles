5b3a11d1bd82e80c40f52a3d51eee7a095544da8
Richard Baptist <code@richardbaptist.nl>
2021-10-21 10:45:52 +0200

    Dynamic resize script
    Increase by 200

--- config/yabai/scripts/resize_windows.sh
+++ config/yabai/scripts/resize_windows.sh
@@ -3,32 +3,34 @@
 function resize_args {
   case "$direction" in
     "left")
-      echo "left:-100:0";;
+      echo "left:-$size:0";;
     "right")
-      echo "right:100:0";;
+      echo "right:$size:0";;
     "up")
-      echo "top:0:-100";;
+      echo "top:0:-$size";;
     "down")
-      echo "bottom:0:100";;
+      echo "bottom:0:$size";;
   esac
 }
 
 function opposite_args {
   case "$direction" in
     "left")
-      echo "right:-100:0";;
+      echo "right:-$size:0";;
     "right")
-      echo "left:100:0";;
+      echo "left:$size:0";;
     "up")
-      echo "bottom:0:-100";;
+      echo "bottom:0:-$size";;
     "down")
-      echo "top:0:100";;
+      echo "top:0:$size";;
   esac
 }
 
 function main {
   local direction
   direction="$1"
+  local size
+  size="$2"
   /usr/local/bin/yabai -m window --resize "$(resize_args)"; /usr/local/bin/yabai -m window --resize "$(opposite_args)"
 }
 
