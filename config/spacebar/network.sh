b3a4c59290c87af3e5920147179d588597ef956d
Richard Baptist <code@richardbaptist.nl>
2022-05-19 14:21:05 +0200

    Add wifi

---
+++ config/spacebar/network.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+output=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I)
+airport=$(echo "$output" | grep 'AirPort' | awk -F': ' '{print $2}')
+
+if [ "$airport" = "Off" ]; then
+  echo "Offline"
+else
+  echo "$(echo "$output" | rg ' SSID' | xargs | awk -F': ' 'echo $2' | sed 's/SSID: //')"
+fi
