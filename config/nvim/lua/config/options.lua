2b48d42ce856fcc54dbdc846e1cb594c5e99028f
Richard Baptist <code@richardbaptist.nl>
2023-07-17 08:40:29 +0200

    Disable swap file

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,9 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.relativenumber = false
+vim.opt.relativenumber = false
+vim.opt.swapfile = false
 
 vim.g.projectionist_heuristics = {
   ["mix.exs"] = {
