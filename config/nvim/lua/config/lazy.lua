02b22d0ef0238af47036fa2fcc80062d5a1588ae
Richard Baptist <code@richardbaptist.nl>
2023-07-17 08:32:01 +0200

    Use animate

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
-    -- { import = "lazyvim.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
 
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
