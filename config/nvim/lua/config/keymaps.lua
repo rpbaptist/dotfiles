d397ddf03b7444b7b23cfa1b840be4967406c54b
Richard Baptist <code@richardbaptist.nl>
2023-07-17 11:00:53 +0200

    Update keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,44 +4,25 @@
 
 -- This file is automatically loaded by lazyvim.config.init
 
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    if opts.remap then
-      opts.remap = nil
-    end
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
-
 vim.keymap.del("n", "<C-Up>")
 vim.keymap.del("n", "<C-Down>")
 vim.keymap.del("n", "<C-Left>")
 vim.keymap.del("n", "<C-Right>")
 
-map("n", "<C-s>", ":w<CR>")
-map("i", "<C-s>", "<ESC>:w<CR>")
+vim.keymap.del({ "n", "i", "v"}, "<A-j>")
+vim.keymap.del({ "n", "i", "v"}, "<A-k>")
 
--- Bind arrow keys to VIM nav
-map({ "n", "x", "v" }, "<Up>", "k", { remap = true })
-map({ "n", "x", "v" }, "<Down>", "j", { remap = true })
-map({ "n", "x", "v" }, "<Left>", "h", { remap = true })
-map({ "n", "x", "v" }, "<Right>", "l", { remap = true })
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+vim.keymap.set("i", "<C-s>", "<ESC>:w<CR>")
 
 -- inoremap <expr> <TAB> pumvisible() ? "<C-y>" : "<TAB>"
 
--- better up/down
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-
 -- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+
+-- Bind arrow keys to VIM nav
+vim.keymap.set({ "n", "x", "v" }, "<Up>", "k", { remap = true })
+vim.keymap.set({ "n", "x", "v" }, "<Down>", "j", { remap = true })
+vim.keymap.set({ "n", "x", "v" }, "<Left>", "h", { remap = true })
+vim.keymap.set({ "n", "x", "v" }, "<Right>", "l", { remap = true })
