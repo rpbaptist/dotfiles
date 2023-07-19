4d031a915794013e056fc63ec5d94e260d9d2ee9
Richard Baptist <code@richardbaptist.nl>
2023-07-19 09:04:36 +0200

    Faster animation

--- config/nvim/lua/plugins/animate.lua
+++ config/nvim/lua/plugins/animate.lua
@@ -10,7 +10,7 @@ return {
           timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
         },
         scroll = {
-          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
           subscroll = animate.gen_subscroll.equal({
             predicate = function(total_scroll)
               if mouse_scrolled then
