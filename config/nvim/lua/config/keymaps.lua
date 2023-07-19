0987bb4bf800da3972da18e5f52ee9d11ce1836d
Richard Baptist <code@richardbaptist.nl>
2023-07-19 09:04:15 +0200

    Better keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -9,15 +9,22 @@ vim.keymap.del("n", "<C-Down>")
 vim.keymap.del("n", "<C-Left>")
 vim.keymap.del("n", "<C-Right>")
 
-vim.keymap.del({ "n", "i", "v"}, "<A-j>")
-vim.keymap.del({ "n", "i", "v"}, "<A-k>")
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
--- vim.keymap.set("n", "<C-s>", ":w<CR>")
--- vim.keymap.set("i", "<C-s>", "<ESC>:w<CR>")
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+
+vim.keymap.set("n", "<A-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<A-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<A-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<A-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Move Lines
-vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("n", "<C-S-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<C-S-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
 
 -- Bind arrow keys to VIM nav
 vim.keymap.set({ "n", "x", "v" }, "<Up>", "k", { remap = true })
@@ -27,6 +34,3 @@ vim.keymap.set({ "n", "x", "v" }, "<Right>", "l", { remap = true })
 
 -- Undo
 vim.keymap.set({ "i", "s" }, "<C-z>", "<cmd>u<cr>", { desc = "Undo" })
-
--- LSP autocomplete
--- inoremap <expr> <TAB> pumvisible() ? "<C-y>" : "<TAB>"
