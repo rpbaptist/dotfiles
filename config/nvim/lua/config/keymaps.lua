e41e439c6594b1f889f285f12c534107bf812535
Richard Baptist <code@richardbaptist.nl>
2023-07-25 11:32:01 +0200

    Use kitty and nvim together

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -37,8 +37,8 @@ vim.keymap.set({ "n", "x", "v" }, "<Right>", "l", { remap = true })
   -- map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
   -- map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
 -- else
-  vim.keymap.set("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-  vim.keymap.set("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+  vim.keymap.set("n", "<C-A-Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+  vim.keymap.set("n", "<C-A-S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 
   vim.keymap.set("n", "<leader>bn", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
   vim.keymap.set("n", "<leader>bi", "<cmd>bnext<cr>", { desc = "Next buffer" })
@@ -58,10 +58,10 @@ vim.keymap.set("n", "<A-S-Right>", "<cmd>vertical resize +10<cr>", { desc = "Inc
 -- vim.keymap.set("n", "<A-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
 -- vim.keymap.set("n", "<A-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
 
-vim.keymap.set("n", "<C-A-u>", ":KittyNavigateUp<cr>", { desc = "Go to upper window", silent = true})
-vim.keymap.set("n", "<C-A-e>", ":KittyNavigateDown<cr>", { desc = "Go to lower window", silent = true})
-vim.keymap.set("n", "<C-A-n>", ":KittyNavigateLeft<cr>", { desc = "Go to left window", silent = true})
-vim.keymap.set("n", "<C-A-i>", ":KittyNavigateRight<cr>", { desc = "Go to right window", silent = true})
+vim.keymap.set("n", "<C-A-u>", ":KittyNavigateUp<cr>", { desc = "Go to upper window", noremap = true, silent = true})
+vim.keymap.set("n", "<C-A-e>", ":KittyNavigateDown<cr>", { desc = "Go to lower window", noremap = true, silent = true})
+vim.keymap.set("n", "<C-A-n>", ":KittyNavigateLeft<cr>", { desc = "Go to left window", noremap = true, silent = true})
+vim.keymap.set("n", "<C-A-i>", ":KittyNavigateRight<cr>", { desc = "Go to right window", noremap = true, silent = true})
 
 -- Move Lines
 vim.keymap.set("n", "<C-S-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -10,11 +10,8 @@ vim.opt.relativenumber = false
 -- let &backupdir=vimtmp
 -- let &directory=vimtmp
 
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "mix"
-vim.g.gruvbox_material_better_performance = 1
-
 vim.g.kitty_navigator_no_mappings = 1
+vim.g.kitty_navigator_password = "nvimwindows"
 
 vim.g.projectionist_heuristics = {
   ["mix.exs"] = {
