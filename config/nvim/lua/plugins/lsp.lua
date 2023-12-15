9c6fd098f6c79025b15aada65cea7666a236427e
Richard Baptist <code@richardbaptist.nl>
2023-12-15 10:51:03 +0100

    Disable custom LSP config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,29 +1,29 @@
 return {
 
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-      })
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    event = "BufReadPre",
-    dependencies = { "mason.nvim" },
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        sources = {
-          nls.builtins.diagnostics.credo,
-          nls.builtins.formatting.prettierd,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.mix,
-        },
-      }
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "stylua",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   event = "BufReadPre",
+  --   dependencies = { "mason.nvim" },
+  --   opts = function()
+  --     local nls = require("null-ls")
+  --     return {
+  --       sources = {
+  --         nls.builtins.diagnostics.credo,
+  --         nls.builtins.formatting.prettierd,
+  --         nls.builtins.formatting.stylua,
+  --         nls.builtins.formatting.mix,
+  --       },
+  --     }
+  --   end,
+  -- },
   -- {
   --   "neovim/nvim-lspconfig",
   --      elixirls = {},
