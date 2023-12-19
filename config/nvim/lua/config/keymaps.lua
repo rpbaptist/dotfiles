ec01fe6add0ac14f6b5ac807987ffe5e5901e7bd
Richard Baptist <richard@remote.com>
2023-12-19 12:15:36 +0100

    Remove LSP keybinds

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -57,22 +57,5 @@ vim.keymap.set(
 )
 
 -- Move Lines
-vim.keymap.set("n", "<C-S-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<C-S-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-
--- Undo
-vim.keymap.set({ "i", "s" }, "<C-z>", "<cmd>u<cr>", { desc = "Undo" })
-
--- LSP
-vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
--- vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.definition() end, opts)
--- vim.keymap.set("n", "<leader>vo", function() vim.lsp.buf.references() end, opts)
--- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
--- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
--- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-
--- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
--- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
--- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
--- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
--- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+vim.keymap.set("i", "<C-S-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("i", "<C-S-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
