00a848de49bcbda79c79383a5cee1e9174af9da2
Richard Baptist <code@richardbaptist.nl>
2024-05-10 09:53:34 +0200

    Add path for mise executables

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,6 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
 
 vim.opt.relativenumber = false
 vim.opt.clipboard = "unnamedplus"
