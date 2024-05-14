a0d1914d6daee247f787005900a217de9e04cfc7
Richard Baptist <code@richardbaptist.nl>
2024-05-14 11:14:42 +0200

    Update NVIM

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -27,6 +27,24 @@ return {
       }
     end,
   },
+  -- {
+  --   "elixir-tools/elixir-tools.nvim",
+  --   version = "*",
+  --   event = { "BufReadPre", "BufNewFile" },
+  --   config = function()
+  --     local elixir = require("elixir")
+  --     local elixirls = require("elixir.elixirls")
+  --
+  --     elixir.setup({
+  --       nextls = { enable = true },
+  --       credo = { enable = true },
+  --       elixirls = { enable = false },
+  --     })
+  --   end,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  -- },
   {
     "neovim/nvim-lspconfig",
     config = function()
