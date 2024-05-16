0e1f8eef8efd7a80945973552b0ae9f1b25eddb3
Richard Baptist <code@richardbaptist.nl>
2024-05-16 12:36:25 +0200

    Update to harpoon2

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -2,25 +2,50 @@ return {
   "nvim-lua/plenary.nvim",
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = function()
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
+      local harpoon = require("harpoon")
 
-      vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add Harpoon mark" })
-      vim.keymap.set("n", "<leader><C-a>", ui.toggle_quick_menu, { desc = "Harpoon toggle menu" })
+      harpoon.setup()
+
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+          })
+          :find()
+      end
+
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+      end, { desc = "Add Harpoon mark" })
+      vim.keymap.set("n", "<leader><C-a>", function()
+        toggle_telescope(harpoon:list())
+      end, { desc = "Harpoon toggle menu" })
 
       vim.keymap.set("n", "<leader><S-1>", function()
-        ui.nav_file(1)
+        harpoon:list():select(1)
       end)
       vim.keymap.set("n", "<leader><S-2>", function()
-        ui.nav_file(2)
+        harpoon:list():select(2)
       end)
       vim.keymap.set("n", "<leader><S-3>", function()
-        ui.nav_file(3)
+        harpoon:list():select(3)
       end)
       vim.keymap.set("n", "<leader><S-4>", function()
-        ui.nav_file(4)
+        harpoon:list():select(4)
       end)
     end,
   },
