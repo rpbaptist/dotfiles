2b5c8fdbab8b1d4d28109c84dc8d6ae2a013a089
Richard Baptist <code@richardbaptist.nl>
2024-05-22 13:44:33 +0200

    Override cmp keymap

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -1,24 +1,44 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  keys = function()
+  event = { "InsertEnter", "CmdlineEnter" },
+
+  opts = function()
     local cmp = require("cmp")
-    return {
-      {
-        "<Tab>",
-        cmp.mapping(function(fallback)
-          if cmp.visible() then
-            if luasnip.expandable() then
-              luasnip.expand()
-            else
-              cmp.confirm({ select = true })
-            end
-          else
-            fallback()
-          end
-        end),
-      },
-    }
-    --
+
+    mapping = cmp.mapping.preset.insert({
+      ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<Tab>"] = LazyVim.cmp.confirm(),
+      ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<C-CR>"] = function(fallback)
+        cmp.abort()
+        fallback()
+      end,
+    })
   end,
+
+  keys = {
+    {
+      "<Down>",
+      function()
+        return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Down>"
+      end,
+      expr = true,
+      silent = true,
+      mode = { "i", "s" },
+    },
+    {
+      "<Up>",
+      function()
+        return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Up>"
+      end,
+      expr = true,
+      silent = true,
+      mode = { "i", "s" },
+    },
+  },
 }
