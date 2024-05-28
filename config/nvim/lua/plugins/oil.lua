c56959ea5b7e1feda904f196fc5b66293887b60a
Richard Baptist <code@richardbaptist.nl>
2024-05-28 13:57:13 +0200

    Define Oil cmd

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Oil",
     opts = {},
     keys = {
       { "<leader>fo", "<cmd>Oil<cr>", desc = "Open parent directory" },
