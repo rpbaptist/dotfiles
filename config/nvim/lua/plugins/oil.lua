930215e15229f50e57959e574f0bc9d96c2a1a3c
Richard Baptist <code@richardbaptist.nl>
2024-05-28 11:17:13 +0200

    Add oil

---
+++ config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    keys = {
+      { "<leader>fo", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    },
+  },
+}
