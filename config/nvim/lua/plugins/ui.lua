91c0c44036fe10cd17fb51cfafb089c8c2bd6b44
Richard Baptist <code@richardbaptist.nl>
2024-05-30 10:38:00 +0200

    Add barbecue

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -24,7 +24,18 @@ return {
       }
     end,
   },
-
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+  },
   {
     "nvim-lualine/lualine.nvim",
     opts = function()
