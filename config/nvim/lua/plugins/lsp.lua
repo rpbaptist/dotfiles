b88112f12f79e2854ea950c64e17d6023ea1c5d5
Richard Baptist <code@richardbaptist.nl>
2024-06-04 13:42:42 +0200

    Try elixirls

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = vim.tbl_filter(
-        function(name)
-          return not vim.tbl_contains({ "elixir-ls" }, name)
-        end,
-
-        vim.list_extend(opts.ensure_installed, {
-          "stylua",
-          "lexical",
-          -- "elixir-ls",
-        })
-      )
-    end,
-  },
-  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
