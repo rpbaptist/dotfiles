be6dc51f9e9593d4b468661cc4563c4433449b4d
Richard Baptist <code@richardbaptist.nl>
2024-06-04 08:21:09 +0200

    Refactor

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -12,110 +12,51 @@ return {
         resize_keys = { "<Left>", "<Down>", "<Up", "<Right>" },
       },
     },
-    keys = {
-      {
-        "<C-A-n>",
-        function()
-          require("smart-splits").move_cursor_left()
-        end,
-        desc = "Move left",
-      },
-      {
-        "<C-A-i>",
-        function()
-          require("smart-splits").move_cursor_right()
-        end,
-        desc = "Move right",
-      },
-      {
-        "<C-A-u>",
-        function()
-          require("smart-splits").move_cursor_up()
-        end,
-        desc = "Move up",
-      },
-      {
-        "<C-A-e>",
-        function()
-          require("smart-splits").move_cursor_down()
-        end,
-        desc = "Move down",
-      },
+    keys = function()
+      local splits = require("smart-splits")
 
-      {
-        "<A-S-n>",
-        function()
-          require("smart-splits").resize_left()
-        end,
-        desc = "Resize left",
-      },
-      {
-        "<A-S-i>",
-        function()
-          require("smart-splits").resize_right()
-        end,
-        desc = "Resize right",
-      },
-      {
-        "<A-S-u>",
-        function()
-          require("smart-splits").resize_up()
-        end,
-        desc = "Resize up",
-      },
-      {
-        "<A-S-e>",
-        function()
-          require("smart-splits").resize_down()
-        end,
-        desc = "Resize down",
-      },
+      return {
+        { "<C-A-n>", splits.move_cursor_left, desc = "Move left" },
+        { "<C-A-i>", splits.move_cursor_right, desc = "Move right" },
+        { "<C-A-u>", splits.move_cursor_up, desc = "Move up" },
+        { "<C-A-e>", splits.move_cursor_down, desc = "Move down" },
 
-      {
-        "<C-A-S-n>",
-        function()
-          require("smart-splits").swap_buf_left({ same_row = true })
-        end,
-        desc = "Swap left",
-      },
-      {
-        "<C-A-S-i>",
-        function()
-          require("smart-splits").swap_buf_right({ same_row = true })
-        end,
-        desc = "Swap right",
-      },
-      {
-        "<C-A-S-u>",
-        function()
-          require("smart-splits").swap_buf_up({ same_row = true })
-        end,
-        desc = "Swap up",
-      },
-      {
-        "<C-A-S-e>",
-        function()
-          require("smart-splits").swap_buf_down({ same_row = true })
-        end,
-        desc = "Swap down",
-      },
+        { "<A-S-n>", splits.resize_left, desc = "Resize left" },
+        { "<A-S-i>", splits.resize_right, desc = "Resize right" },
+        { "<A-S-u>", splits.resize_up, desc = "Resize up" },
+        { "<A-S-e>", splits.resize_down, desc = "Resize down" },
 
-      {
-        "<A-S-r>",
-        function()
-          require("smart-splits").start_resize_mode()
-        end,
-        desc = "Resize mode",
-      },
-    },
+        {
+          "<C-A-S-n>",
+          function()
+            splits.swap_buf_left({ same_row = true })
+          end,
+          desc = "Swap left",
+        },
+        {
+          "<C-A-S-i>",
+          function()
+            splits.swap_buf_right({ same_row = true })
+          end,
+          desc = "Swap right",
+        },
+        {
+          "<C-A-S-u>",
+          function()
+            splits.swap_buf_up({ same_row = true })
+          end,
+          desc = "Swap up",
+        },
+        {
+          "<C-A-S-e>",
+          function()
+            splits.swap_buf_down({ same_row = true })
+          end,
+          desc = "Swap down",
+        },
+
+        { "<A-S-r>", splits.start_resize_mode, desc = "Resize mode" },
+      }
+    end,
   },
-  -- {
-  --   "knubie/vim-kitty-navigator",
-  --   keys = {
-  --     { "<C-A-u>", "<cmd>KittyNavigateUp<cr>" },
-  --     { "<C-A-e>", "<cmd>KittyNavigateDown<cr>" },
-  --     { "<C-A-n>", "<cmd>KittyNavigateLeft<cr>" },
-  --     { "<C-A-i>", "<cmd>KittyNavigateRight<cr>" },
-  --   },
-  -- },
 }
