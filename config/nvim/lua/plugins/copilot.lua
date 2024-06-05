acdea8bd3530bb17a1d3d4f9cea6026cde06ad8f
Richard Baptist <code@richardbaptist.nl>
2024-06-05 10:51:20 +0200

    Use Copilot chat extra

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,80 +1,80 @@
 return {
-  { "copilot-cmp", enabled = false },
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = {
-        auto_trigger = false,
-      },
-      panel = {
-        auto_refresh = true,
-      },
-    },
-    filetypes = {
-      markdown = false,
-    },
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    build = function()
-      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-    end,
-    event = "VeryLazy",
-    keys = {
-      { "<leader>ccb", ":CopilotChatBuffer ", desc = "CopilotChat - Chat with current buffer" },
-      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-      {
-        "<leader>ccT",
-        "<cmd>CopilotChatVsplitToggle<cr>",
-        desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
-      },
-      {
-        "<leader>ccv",
-        ":CopilotChatVisual ",
-        mode = "x",
-        desc = "CopilotChat - Open in vertical split",
-      },
-      {
-        "<leader>ccx",
-        ":CopilotChatInPlace<cr>",
-        mode = "x",
-        desc = "CopilotChat - Run in-place code",
-      },
-      {
-        "<leader>ccf",
-        "<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
-        desc = "CopilotChat - Fix diagnostic",
-      },
-      {
-        "<leader>ccr",
-        "<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
-        desc = "CopilotChat - Reset chat history and clear buffer",
-      },
-    },
-    cmd = {
-      "CopilotChat",
-      "CopilotChatOpen",
-      "CopilotChatClose",
-      "CopilotChatToggle",
-      "CopilotChatReset",
-      "CopilotChatSave",
-      "CopilotChatLoad",
-      "CopilotChatDebugInfo",
-      "CopilotChatExplain",
-      "CopilotChatReview",
-      "CopilotChatFix",
-      "CopilotChatOptimize",
-      "CopilotChatDocs",
-      "CopilotChatTests",
-      "CopilotChatFixDiagnostic",
-      "CopilotChatCommit",
-      "CopilotChatCommitStaged",
-    },
-  },
+  -- { "copilot-cmp", enabled = false },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   opts = {
+  --     suggestion = {
+  --       auto_trigger = false,
+  --     },
+  --     panel = {
+  --       auto_refresh = true,
+  --     },
+  --   },
+  --   filetypes = {
+  --     markdown = false,
+  --   },
+  -- },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   branch = "canary",
+  --   dependencies = {
+  --     { "zbirenbaum/copilot.lua" },
+  --     { "nvim-lua/plenary.nvim" },
+  --   },
+  --   build = function()
+  --     vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+  --   end,
+  --   event = "VeryLazy",
+  --   keys = {
+  --     { "<leader>ccb", ":CopilotChatBuffer ", desc = "CopilotChat - Chat with current buffer" },
+  --     { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+  --     { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+  --     {
+  --       "<leader>ccT",
+  --       "<cmd>CopilotChatVsplitToggle<cr>",
+  --       desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
+  --     },
+  --     {
+  --       "<leader>ccv",
+  --       ":CopilotChatVisual ",
+  --       mode = "x",
+  --       desc = "CopilotChat - Open in vertical split",
+  --     },
+  --     {
+  --       "<leader>ccx",
+  --       ":CopilotChatInPlace<cr>",
+  --       mode = "x",
+  --       desc = "CopilotChat - Run in-place code",
+  --     },
+  --     {
+  --       "<leader>ccf",
+  --       "<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
+  --       desc = "CopilotChat - Fix diagnostic",
+  --     },
+  --     {
+  --       "<leader>ccr",
+  --       "<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
+  --       desc = "CopilotChat - Reset chat history and clear buffer",
+  --     },
+  --   },
+  --   cmd = {
+  --     "CopilotChat",
+  --     "CopilotChatOpen",
+  --     "CopilotChatClose",
+  --     "CopilotChatToggle",
+  --     "CopilotChatReset",
+  --     "CopilotChatSave",
+  --     "CopilotChatLoad",
+  --     "CopilotChatDebugInfo",
+  --     "CopilotChatExplain",
+  --     "CopilotChatReview",
+  --     "CopilotChatFix",
+  --     "CopilotChatOptimize",
+  --     "CopilotChatDocs",
+  --     "CopilotChatTests",
+  --     "CopilotChatFixDiagnostic",
+  --     "CopilotChatCommit",
+  --     "CopilotChatCommitStaged",
+  --   },
+  -- },
 }
