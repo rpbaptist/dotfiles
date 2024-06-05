5d810cf928316ea10c788fa934b75b57919e64ad
Richard Baptist <code@richardbaptist.nl>
2024-06-05 12:58:39 +0200

    Better multicursor

---
+++ config/nvim/lua/plugins/multicursor.lua
@@ -0,0 +1,20 @@
+return {
+  -- lazy.nvim:
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "smoka7/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<C-n>",
+        "<cmd>MCstart<cr>",
+        desc = "Start multicursor",
+      },
+    },
+  },
+}
