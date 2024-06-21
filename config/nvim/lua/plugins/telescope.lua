2b80ccdbb3ea0015505661f4e459ed99199c5893
Richard Baptist <code@richardbaptist.nl>
2024-06-21 08:36:26 +0200

    Only include overrides

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,8 @@
 return {
   "telescope.nvim",
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
     "jedrzejboczar/possession.nvim",
-    build = "make",
     config = function()
-      require("telescope").load_extension("fzf")
       require("telescope").load_extension("possession")
     end,
   },
