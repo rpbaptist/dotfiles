cfcaf0cf8f0f1b6f7f437c5d33f0fd6a174a0add
Richard Baptist <code@richardbaptist.nl>
2024-07-03 09:21:38 +0200

    Grep current dir

--- config/nvim/lua/plugins/picker.lua
+++ config/nvim/lua/plugins/picker.lua
@@ -1,44 +1,30 @@
 return {
+  { "telescope.nvim", enabled = false },
   {
-
-  "telescope.nvim",
-  enabled = false,
-  -- dependencies = {
-  --   "jedrzejboczar/possession.nvim",
-  --   config = function()
-  --     require("telescope").load_extension("possession")
-  --   end,
-  -- },
-  -- opts = {
-  --   defaults = {
-  --     file_ignore_patterns = { "lib/eor/*", "test/eor/*" },
-  --   },
-  -- },
-  -- keys = {
-  --   { "<leader>gs", false },
-  --   { "<leader>gc", false },
-  --   { "<leader>sR", false },
-  --   { "<leader>gf", "<cmd>Telescope git_status<cr>", desc = "Git files" },
-  --   { "<leader>bl", "<cmd>Telescope buffers<cr>", desc = "List buffers" },
-  -- },
-},
-  { "ibhagwan/fzf-lua",
+    "ibhagwan/fzf-lua",
     opts = {
-    defaults = {
+      defaults = {
         file_ignore_patterns = { "lib/eor/*", "test/eor/*" },
         no_header = true
       },
     },
     keys = {
 
-     { "<leader>gb", false },
-     { "<leader>gc", false },
-     { "<leader>gs", false },
-     { "<leader>gb", "<cmd>FzfLua git_bcommits<cr>", desc = "Git blame"},
-     { "<leader>gl", "<cmd>FzfLua git_commits<cr>", desc = "Git log"},
-     { "<leader>go", "<cmd>FzfLua git_branches<cr>", desc = "Git checkout"},
-     { "<leader>gf", "<cmd>FzfLua git_status<cr>", desc = "Git files" },
-     { "<leader>bl", "<cmd>FzfLua buffers<cr>", desc = "List buffers" },
-    }
-  }
+      { "<leader>gb", false },
+      { "<leader>gc", false },
+      { "<leader>gs", false },
+      { "<leader>gb", "<cmd>FzfLua git_bcommits<cr>", desc = "Git blame" },
+      { "<leader>gl", "<cmd>FzfLua git_commits<cr>", desc = "Git log" },
+      { "<leader>go", "<cmd>FzfLua git_branches<cr>", desc = "Git checkout" },
+      { "<leader>gf", "<cmd>FzfLua git_status<cr>", desc = "Git files" },
+      { "<leader>bl", "<cmd>FzfLua buffers<cr>", desc = "List buffers" },
+      {
+        "<leader>sG",
+        function()
+          require("fzf-lua").live_grep({ cwd = vim.fn.expand("%:h") })
+        end,
+        desc = "Grep current dir",
+      },
+    },
+  },
 }
