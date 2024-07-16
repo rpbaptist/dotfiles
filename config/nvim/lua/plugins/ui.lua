d4af85183463e71d39b25c8553be46960fc88fbd
Richard Baptist <code@richardbaptist.nl>
2024-07-16 15:39:38 +0200

    Use tabby

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -53,6 +53,45 @@ return {
     },
   },
   {
+    "nanozuki/tabby.nvim",
+    opts = {
+      line = function(line)
+        local theme = {
+          fill = "TabLineFill",
+          head = "TabLine",
+          current_tab = "TabLineSel",
+          tab = "TabLine",
+          win = "TabLine",
+          tail = "TabLine",
+        }
+        return {
+          {
+            { hl = theme.head },
+            line.sep("", theme.head, theme.fill),
+          },
+          line.tabs().foreach(function(tab)
+            local hl = tab.is_current() and theme.current_tab or theme.tab
+            return {
+              line.sep("", hl, theme.fill),
+              tab.is_current() and "" or "",
+              tab.number(),
+              tab.name(),
+              line.sep("", hl, theme.fill),
+              hl = hl,
+              margin = " ",
+            }
+          end),
+          line.spacer(),
+          {
+            line.sep("", theme.tail, theme.fill),
+            { "  ", hl = theme.tail },
+          },
+          hl = theme.fill,
+        }
+      end,
+    },
+  },
+  {
     "nvim-lualine/lualine.nvim",
     opts = {
       sections = {
