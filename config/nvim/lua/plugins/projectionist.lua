49f125c4ff2193571218ebec4076beef98432373
Richard Baptist <code@richardbaptist.nl>
2024-09-02 09:10:41 +0200

    Update lazy

--- config/nvim/lua/plugins/projectionist.lua
+++ config/nvim/lua/plugins/projectionist.lua
@@ -8,6 +8,9 @@ return {
     "AV",
   },
   keys = {
-    { "<leader>tf", "<cmd>A<cr>", desc = "Jump to test file" },
+    { "<leader>tff", "<cmd>A<cr>", desc = "Jump to test file" },
+    { "<leader>tfe", "<cmd>AS<cr>", desc = "Open test file in hsplit" },
+    { "<leader>tfi", "<cmd>AV<cr>", desc = "Open test file in vsplit" },
+    { "<leader>tf<Tab>", "<cmd>AT<cr>", desc = "Open test file in tab" },
   },
 }
