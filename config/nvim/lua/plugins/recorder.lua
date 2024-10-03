6d49a5ebf08ed519a4d85803b3dd45a58a6e4621
Richard Baptist <code@richardbaptist.nl>
2024-10-03 14:36:32 +0200

    Better keybinds

--- config/nvim/lua/plugins/recorder.lua
+++ config/nvim/lua/plugins/recorder.lua
@@ -3,5 +3,19 @@ return {
   depencencies = { "rcarriga/nvim-notify" },
   opts = {
     lessNotifications = true,
+    mapping = {
+      startStopRecording = "<leader>rr",
+      switchSlot = "<leader>rs",
+      editMacro = "<leader>re",
+      deleteAllMacros = "<leader>rd",
+      yankMacro = "<leader>ry",
+    },
+  },
+  keys = {
+    { "<leader>rr", desc = "Start/stop recording" },
+    { "<leader>rs", desc = "Switch slot" },
+    { "<leader>re", desc = "Edit macro" },
+    { "<leader>rd", desc = "Delete all macros" },
+    { "<leader>ry", desc = "Yank macro" },
   },
 }
