3acc7bb495625bdb0ca545a457a33e876690f33e
Richard Baptist <code@richardbaptist.nl>
2024-10-24 09:15:33 +0200

    Mix env dev

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,24 @@
 return {
-  "nvimtools/none-ls.nvim",
-  opts = function(_, opts)
-    local remove_sources = { "credo" }
-    opts.sources = vim.tbl_filter(function(source)
-      return not vim.tbl_contains(remove_sources, source.name)
-    end, opts.sources)
-  end,
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local remove_sources = { "credo" }
+      opts.sources = vim.tbl_filter(function(source)
+        return not vim.tbl_contains(remove_sources, source.name)
+      end, opts.sources)
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      vim.tbl_deep_extend("error", opts.servers.elixirls, {
+        settings = {
+          elixirLS = {
+            mixEenv = "dev",
+            mixTarget = "dev",
+          },
+        },
+      })
+    end,
+  },
 }
